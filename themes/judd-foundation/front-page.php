f8444335373433edb3af364828c2c2b0091d37b8
Luke Weston <weston@teamsubjectmatter.com>
2016-04-03 17:56:32 -0400

    Removed node_modules folder, added it to .gitignore, added some homepage stuff

--- themes/judd-foundation/front-page.php
+++ themes/judd-foundation/front-page.php
@@ -14,31 +14,7 @@
 
 get_header('home'); ?>
 
-<style>
-	.home {
-	@include background-image(url("images/judd-foundation-homepage-image.jpg"));
-  	background-size: cover;
-}
-</style>
-
-	<div id="primary" class="content-area">
+	<div id="primary" >
 		<main id="main" class="site-main" role="main">
-
-			<?php
-			while ( have_posts() ) : the_post();
-
-				get_template_part( 'template-parts/content', 'page' );
-
-				// If comments are open or we have at least one comment, load up the comment template.
-				if ( comments_open() || get_comments_number() ) :
-					comments_template();
-				endif;
-
-			endwhile; // End of the loop.
-			?>
-
-		</main><!-- #main -->
+		</main><!-- #main -->		
 	</div><!-- #primary -->
-
-<?php
-get_footer('home');
