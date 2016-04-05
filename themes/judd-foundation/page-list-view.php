dd01fdb3267cdff165bc83febfaa7282dec7b141
Intern <intern@wdchfc201.homefront.local>
2016-04-05 13:53:02 -0400

    list page edit

--- themes/judd-foundation/page-list-view.php
+++ themes/judd-foundation/page-list-view.php
@@ -18,7 +18,7 @@ get_header(); ?>
 	<?php
 		while ( have_posts() ) : the_post();
 
-			get_template_part( 'template-parts/content', 'list' );
+			get_template_part( 'template-parts/content', 'list-view' );
 
 			// If comments are open or we have at least one comment, load up the comment template.
 			if ( comments_open() || get_comments_number() ) :
