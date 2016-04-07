f6f2668ec82b71c2fe6ec05ddb5ddb83c57663a4
Luke Weston <weston@teamsubjectmatter.com>
2016-04-07 11:43:15 -0400

    testing push hook

--- themes/judd-foundation/page-section-landing.php
+++ themes/judd-foundation/page-section-landing.php
@@ -10,5 +10,5 @@ while ( have_posts() ) : the_post();
 	get_template_part( 'template-parts/content', 'hero' );
 	get_template_part( 'template-parts/content', 'landing' );
 endwhile; 
-
+//get footer
 get_footer();
