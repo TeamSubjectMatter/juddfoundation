49198e6a09db11611f8c74fb58f26b4c14c7b76d
Luke Weston <weston@teamsubjectmatter.com>
2016-04-07 11:33:05 -0400

    added post push hook

---
+++ themes/judd-foundation/template-parts/content-single-content.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Template part for displaying posts.
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package judd-foundation
+ */
+
+?>
+
+<article id="post-<?php the_ID(); ?>" class="singleContent">
+
+<!--	
+	<section class="page-content-area-full">
+		<?php the_post_thumbnail(); ?>
+	</section>
+-->
+
+	<section class="page-content-area">
+		<h1><?php the_title(); ?></h1>
+		<h2><?php the_content(); ?></h2>
+	</section>
+
+	<section class="right-sidebar">
+	<?php while( have_rows('sidebar_content_blocks') ): the_row(); ?>
+		<div class="right-side-images">
+        	<img src="<?php the_sub_field('image'); ?>" class="img-responsive" alt="">
+        </div>
+    <?php endwhile; ?>
+	</section>
+
+</article>
