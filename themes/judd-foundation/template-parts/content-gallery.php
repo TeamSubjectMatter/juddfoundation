4e18097e22323217fddb3a3b91ac417d0a524ed3
Luke Weston <weston@teamsubjectmatter.com>
2016-04-09 08:43:50 -0400

    spaces

--- themes/judd-foundation/template-parts/content-gallery.php
+++ themes/judd-foundation/template-parts/content-gallery.php
@@ -10,28 +10,34 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	
 	<h1><?php the_title(); ?></h1>
-
 	<?php the_content(); ?>
-
 </article>
 
 <article>
-	<div class-"gallery-view">
-	<?php if( have_rows('gallery_view') ): ?>
-    <?php while ( have_rows('gallery_view') ) : the_row(); ?>  
-		<?php $post_object = get_sub_field('gallery_item'); ?>
-        <?php if( $post_object ): ?>
-        <?php $post = $post_object; setup_postdata( $post ); ?>
-        <a href="<?php the_permalink(); ?>" class="">
-        <?php the_post_thumbnail('', array('class' => 'img-responsive')); ?></a>
-        <?php wp_reset_postdata(); ?>
-        <?php endif; ?>
-	<?php endwhile; ?>
-    <?php endif; ?>
+
+<?php 
+echo get_post_field( 'post_name', get_post() );
+	// query custom post types based on page slug 
+	query_posts( array( 
+					
+					'post_type' => array(get_post_field( 'post_name', get_post() ) ),
+					'showposts' => 10
+				 ) );
+	while(have_posts()) : the_post(); 
+
+	//get thumbnail URL
+	$thumb_id = get_post_thumbnail_id();
+	$thumb_url_array = wp_get_attachment_image_src($thumb_id, 'thumbnail-size', true);
+	$thumb_url = $thumb_url_array[0];
+?>
+	<div class="block-4">
+		<a href="<?php the_permalink(); ?>" class="">
+        	<img src="<?= $thumb_url; ?>">
+			<div class="overlay">
+				<p><?php the_title(); ?></p>
+			</div>
+        </a>
 	</div>
+<?php endwhile; ?>
 </article>
-
-
-
