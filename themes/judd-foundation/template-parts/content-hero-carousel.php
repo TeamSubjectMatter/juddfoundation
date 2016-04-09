33f58a6ff90e40f32277f0f379a5e056de28fa4c
Luke Weston <weston@teamsubjectmatter.com>
2016-04-09 10:15:09 -0400

    spaces

--- themes/judd-foundation/template-parts/content-hero-carousel.php
+++ themes/judd-foundation/template-parts/content-hero-carousel.php
@@ -8,11 +8,6 @@
 
 
 ?>
-<div class="hero" style='background-image: url("<?php echo the_field('hero_image'); ?>");'>
-</div>
-
-
-
 <div id="jssor_1" style="position: relative; margin: 0 auto; top: 0px; left: 0px; width: 1300px; height: 500px; overflow: hidden; visibility: hidden;">
     <!-- Loading Screen -->
     <div data-u="loading" style="position: absolute; top: 0px; left: 0px;">
