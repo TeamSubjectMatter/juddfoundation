46db229a6086b2fab7441d07ff8fe0956dad95b6
Luke Weston <weston@teamsubjectmatter.com>
2016-04-09 12:56:29 -0400

    spaces

--- themes/judd-foundation/template-parts/content-single-content.php
+++ themes/judd-foundation/template-parts/content-single-content.php
@@ -20,17 +20,12 @@
 	</section>
 
 	<section class="right-sidebar">
-    <?php
-
-	$post_objects = get_field('sidebar_content_blocks');
-	
-	if( $post_objects ): ?>
+	<?php if( have_rows('sidebar_content_blocks') ): ?>
 		<ul>
-		<?php foreach( $post_objects as $post):  ?>
-			<?php setup_postdata($post); ?>
-			<?php echo $post->ID; ?>
+	
+		<?php while( have_rows('sidebar_content_blocks') ): the_row();  ?>
 			<li>
-				<?php $image = wp_get_attachment_image_src( get_post_thumbnail_id( ), 'single-post-thumbnail' ); ?>
+				<?php $image = wp_get_attachment_image_src( get_post_thumbnail_id(), 'single-post-thumbnail' ); ?>
 				<a class="right-side-images" href="<?php the_permalink(); ?>"><img src="<?php echo $image[0]; ?>"></a>
 			</li>
 		<?php endforeach; ?>
