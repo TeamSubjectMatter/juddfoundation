f3b045cd2e3b5aa67f1ce67f74943c7d57bc617f
Luke Weston <weston@teamsubjectmatter.com>
2016-04-09 10:20:37 -0400

    spaces

--- themes/judd-foundation/functions.php
+++ themes/judd-foundation/functions.php
@@ -497,6 +497,7 @@ add_action( 'init', 'add_custom_taxonomies', 0 );
 function judd_foundation_scripts() {
 	wp_enqueue_style( 'judd-foundation-style', get_stylesheet_uri() );
 	wp_enqueue_style('font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css'); 
+	wp_enqueue_script( 'judd-foundation-slider', get_template_directory_uri() . '/js/jssor.js', array(), '20120206', true );
 	wp_enqueue_script( 'judd-foundation-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20120206', true );
 	wp_enqueue_script( 'judd-foundation-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20130115', true );
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
