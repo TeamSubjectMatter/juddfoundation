e41958fcf63d845ea20689672993bd7e9c8d68d2
Luke Weston <weston@teamsubjectmatter.com>
2016-04-09 09:50:07 -0400

    spaces

--- themes/judd-foundation/template-parts/content-gallery-b.php
+++ themes/judd-foundation/template-parts/content-gallery-b.php
@@ -11,10 +11,7 @@
 
 <article id="post-<?php the_ID(); ?>" class="galleryViewB">
 	<h1><?php the_title(); ?></h1>
-	<?php var_dump($post); ?>
-	<?php echo get_page_template_slug( $post->ID ); ?>
-	<?php the_content(); ?>
-	here
+	<?php get_the_content(); ?>
 </article>
 
 <article>
