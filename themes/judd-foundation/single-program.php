e402a91666fe4922324c12f1fa4a84f8b8af3869
Luke Weston <weston@teamsubjectmatter.com>
2016-04-11 14:53:45 -0400

    list view page

---
+++ themes/judd-foundation/single-program.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * The template for displaying all pages.
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site may use a
+ * different template.
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ * 
+ * @package judd-foundation
+ */
+
+get_header(); 
+
+while ( have_posts() ) : the_post();
+
+	get_template_part( 'template-parts/content', 'hero-carousel' );
+	get_template_part( 'template-parts/content', 'single-content' );
+
+endwhile; // End of the loop.
+
+get_footer();
