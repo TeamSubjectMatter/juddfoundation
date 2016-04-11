4cf5ab4451cd0b8c8d968c404d77164b1a108e8c
Luke Weston <weston@teamsubjectmatter.com>
2016-04-11 14:51:34 -0400

    list view page

--- themes/judd-foundation/template-parts/content-list.php
+++ themes/judd-foundation/template-parts/content-list.php
@@ -18,7 +18,6 @@ query_posts( array(
 				'post_type' => array(get_post_field( 'post_name', get_post() ) )
 			 ) );
 while(have_posts()) : the_post(); 
-
 	$thumb_id = get_post_thumbnail_id();
 	$thumb_url_array = wp_get_attachment_image_src($thumb_id, 'thumbnail-size', true);
 	$thumb_url = $thumb_url_array[0];
@@ -27,8 +26,10 @@ while(have_posts()) : the_post();
 	<div class="hero" style='background-image: url("<?php echo $thumb_url; ?>");'></div>
 
 	<h2><?php the_title(); ?></h2>
-	<?php the_content(); ?>
-	<p><a href="<?php the_permalink(); ?>">Read More</a></p>
+	<?php the_excerpt(); ?>
+	<p>
+		<a href="<?php the_permalink(); ?>">Read More</a>
+	</p>
 <?php endwhile; ?>
 		
 </article>
