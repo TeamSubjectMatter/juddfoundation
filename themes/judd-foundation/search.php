a2c708442420ea7b19a1b92be11123a46a671607
Luke Weston <weston@teamsubjectmatter.com>
2016-04-12 13:15:06 -0400

    thumbnails

--- themes/judd-foundation/search.php
+++ themes/judd-foundation/search.php
@@ -7,42 +7,13 @@
  * @package judd-foundation
  */
 
-get_header(); ?>
+get_header(); 
 
-	<section id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
+while ( have_posts() ) : the_post();
 
-		<?php
-		if ( have_posts() ) : ?>
+	get_template_part( 'template-parts/content', 'hero' );
+	get_template_part( 'template-parts/content', 'search' );
 
-			<header class="page-header">
-				<h1 class="page-title"><?php printf( esc_html__( 'Search Results for: %s', 'judd-foundation' ), '<span>' . get_search_query() . '</span>' ); ?></h1>
-			</header><!-- .page-header -->
+endwhile; 
 
-			<?php
-			/* Start the Loop */
-			while ( have_posts() ) : the_post();
-
-				/**
-				 * Run the loop for the search to output the results.
-				 * If you want to overload this in a child theme then include a file
-				 * called content-search.php and that will be used instead.
-				 */
-				get_template_part( 'template-parts/content', 'search' );
-
-			endwhile;
-
-			the_posts_navigation();
-
-		else :
-
-			get_template_part( 'template-parts/content', 'none' );
-
-		endif; ?>
-
-		</main><!-- #main -->
-	</section><!-- #primary -->
-
-<?php
-get_sidebar();
 get_footer();
