c41a8386495ec916ebfb430f1716c355cf6ee66b
teia atkins <atkins@teamsubjectmatter.com>
2016-04-12 16:06:15 -0400

    changed images to background on section landing

--- themes/judd-foundation/template-parts/content-landing.php
+++ themes/judd-foundation/template-parts/content-landing.php
@@ -17,7 +17,17 @@
 	
 	<?php while( have_rows('content_blocks') ): the_row(); ?>
 	<section class="block-3">
-		<a href="<?php echo get_sub_field('link');?>"><img src="<?= get_sub_field('image'); ?>" class="img-responsive" alt="<?= "" ?>" /></a>
+		
+		<div class="img-contain">
+			<a href="<?php echo get_sub_field('link');?>">
+				
+				<div class="img-thumb" style="background:url(' <?php echo get_sub_field("image");?>');    background-size: cover;
+							    background-repeat: no-repeat;
+							    background-position: center center;">
+							</div>
+			</a>
+		</div>
+
 		<h3><?= get_sub_field('heading'); ?></h3>
 		<h4><?= get_sub_field('text'); ?></h4>
 		<h5><a href="<?php echo get_sub_field('link');?>" class="gold">Read More</a></h5>
