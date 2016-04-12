3a1caf79e021a803b61316bfff90a145812d369a
teia atkins <atkins@teamsubjectmatter.com>
2016-04-12 10:28:49 -0400

    added content to spaces

--- themes/judd-foundation/template-parts/content-gallery-b.php
+++ themes/judd-foundation/template-parts/content-gallery-b.php
@@ -11,7 +11,7 @@
 
 <article id="post-<?php the_ID(); ?>" class="galleryViewB">
 	<h1><?php the_title(); ?></h1>
-	<h2><?php the_content(); ?></h2>
+	<h2><?php echo wpautop($post->post_content);?></h2>
 </article>
 
 <article>
