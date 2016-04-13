54a955025795d2fc6411b58f151d8f79bd6a199c
teia atkins <atkins@teamsubjectmatter.com>
2016-04-13 15:17:41 -0400

    fixed search

--- themes/judd-foundation/template-parts/content-none.php
+++ themes/judd-foundation/template-parts/content-none.php
@@ -24,13 +24,13 @@
 
 			<p><?php esc_html_e( 'Sorry, but nothing matched your search terms. Please try again with some different keywords.', 'judd-foundation' ); ?></p>
 			<?php
-				get_search_form();
+				//get_search_form();
 
 		else : ?>
 
 			<p><?php esc_html_e( 'It seems we can&rsquo;t find what you&rsquo;re looking for. Perhaps searching can help.', 'judd-foundation' ); ?></p>
 			<?php
-				get_search_form();
+				//get_search_form();
 
 		endif; ?>
 	</div><!-- .page-content -->
--- themes/judd-foundation/template-parts/content-search.php
+++ themes/judd-foundation/template-parts/content-search.php
@@ -1,3 +1,4 @@
+
 <?php
 /**
  * Template part for displaying results in search pages.
@@ -6,49 +7,21 @@
  *
  * @package judd-foundation
  */
-echo var_dump($post);
 ?>
 
-<div id="jssor_1" style="position: relative; margin: 0 auto; top: 0px; left: 0px; width: 1300px; height: 500px; overflow: hidden; visibility: hidden;">
-    <!-- Loading Screen -->
-    <div data-u="loading" style="position: absolute; top: 0px; left: 0px;">
-        <div style="filter: alpha(opacity=70); opacity: 0.7; position: absolute; display: block; top: 0px; left: 0px; width: 100%; height: 100%;"></div>
-        <div style="position:absolute;display:block;background:url('<?= get_template_directory_uri (); ?>/img/loading.gif') no-repeat center center;top:0px;left:0px;width:100%;height:100%;"></div>
-    </div>
-    <div data-u="slides" style="cursor: default; position: relative; top: 0px; left: 0px; width: 1300px; height: 500px; overflow: hidden;">
-		<!-- get featured image -->
-		<?php
-			$thumb_id = get_post_thumbnail_id(get_option( 'page_on_front' ));
-			$thumb_url_array = wp_get_attachment_image_src($thumb_id, 'thumbnail-size', true);
-			$thumb_url = $thumb_url_array[0];
-		?>
-		<div data-p="225.00">
-            <img data-u="image" src="<?php echo $thumb_url; ?>" style="height: 100%; width: auto;"/>
-        </div>
-    </div>
-    <!-- Arrow Navigator -->
-    <span data-u="arrowleft" class="jssora22l" style="top:0px;left:12px;width:40px;height:58px;" data-autocenter="2"></span>
-    <span data-u="arrowright" class="jssora22r" style="top:0px;right:12px;width:40px;height:58px;" data-autocenter="2"></span>
-</div>
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+    <header class="entry-header">
+        <?php the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
+    
+        <?php if ( 'post' === get_post_type() ) : ?>
+        <div class="entry-meta">
+            <?php judd_foundation_posted_on(); ?>
+        </div><!-- .entry-meta -->
+        <?php endif; ?>
+    </header><!-- .entry-header -->
 
-<article id="">
+    <div class="entry-summary">
+        <?php the_excerpt(); ?>
+    </div><!-- .entry-summary -->
 
-<?php
-global $query_string;
-
-$query_args = explode("&", $query_string);
-$search_query = array();
-
-if( strlen($query_string) > 0 ) {
-	foreach($query_args as $key => $string) {
-		$query_split = explode("=", $string);
-		$search_query[$query_split[0]] = urldecode($query_split[1]);
-	} // foreach
-} //if
-
-$search = new WP_Query($search_query);
-$posts = $search->get_posts();
-
-
-?>
 </article><!-- #post-## -->
