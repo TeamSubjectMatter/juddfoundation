ad4e1f01ce6fd1708b27e05790b8a2d36a8810d3
teia atkins <atkins@teamsubjectmatter.com>
2016-04-20 12:22:20 -0400

    style update

--- themes/judd-foundation/functions.php
+++ themes/judd-foundation/functions.php
@@ -209,7 +209,7 @@ function custom_post_type() {
 		'show_in_admin_bar'   => true,
 		'menu_position'       => 6,
 		'can_export'          => true,
-		'has_archive'         => true,
+		'has_archive'         => false,
 		'exclude_from_search' => false,
 		'publicly_queryable'  => true,
 		'capability_type'     => 'post',
